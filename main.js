c5976a19a9279f209ed7449a00af49d5ca567eef
zoya1601 <90971661+zoya1601@users.noreply.github.com>
2022-02-16 19:01:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("welcome");
+var a="zoya";
+var b="roza sultana";
+var c=a.concat(b);
+console.log("first name",a);
+console.log("last name",b);
+console.log("fullname",c);
+var d=c.length;
+console.log("length of the word",d);
